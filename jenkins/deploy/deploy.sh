cf64900cd745e8c6e3396ea98dc341efcb5d3d42
subhash1992 <47549520+subhash1992@users.noreply.github.com>
2019-09-12 22:30:23 +0530

    Update deploy.sh

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo maven-project > /tmp/.auth
-echo $BUILD_TAG >> /tmp/.auth
-echo $PASS >> /tmp/.auth
+echo maven-project > /root/jenkins-pipeline/jenkins_home/tmp/.auth
+echo $BUILD_TAG >> /root/jenkins-pipeline/jenkins_home/tmp/.auth
+echo $PASS >> /root/jenkins-pipeline/jenkins_home/tmp/.auth
 
-scp -i /root/jenkins-pipeline/jenkins_home/.ssh/prod /tmp/.auth root@13.233.30.180:/tmp/.auth
-scp -i /root/jenkins-pipeline/jenkins_home/.ssh/prod ./jenkins/deploy/publish root@13.233.30.180:/tmp/publish
+scp -i /root/jenkins-pipeline/jenkins_home/.ssh/prod /root/jenkins-pipeline/jenkins_home/tmp/.auth root@13.233.30.180:/tmp/.auth
+scp -i /root/jenkins-pipeline/jenkins_home/.ssh/prod /root/jenkins-pipeline/jenkins_home/jobs/pipeline-docker-maven/workspace/jenkins/deploy/publish root@13.233.30.180:/tmp/publish
 ssh -i /root/jenkins-pipeline/jenkins_home/.ssh/prod root@13.233.30.180 "/tmp/publish"
 
