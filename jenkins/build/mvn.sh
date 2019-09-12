eaf3a038d4a821367699add7f9cc8b68b5fa5bf2
subhash1992 <47549520+subhash1992@users.noreply.github.com>
2019-09-12 18:03:04 +0530

    second commit

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,7 +4,7 @@ echo "***************************"
 echo "** Building jar ***********"
 echo "***************************"
 
-WORKSPACE=/home/jenkins/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
+WORKSPACE=/var/jenkins_home/jobs/pipeline-docker-maven/workspace
 
 docker run --rm  -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
 
