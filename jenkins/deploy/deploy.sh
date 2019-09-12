5a778e38d21ddf02dedc98d47c0abf16eae3fb9a
subhash1992 <47549520+subhash1992@users.noreply.github.com>
2019-09-12 22:12:11 +0530

    Update deploy.sh

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -4,7 +4,7 @@ echo maven-project > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
-scp -i /var/jenkins_home/.ssh/prod /tmp/.auth root@13.127.132.53:/tmp/.auth
-scp -i /var/jenkins_home/.ssh/prod ./jenkins/deploy/publish root@13.127.132.53:/tmp/publish
-ssh -i /var/jenkins_home/.ssh/prod root@13.127.132.53 "/tmp/publish"
+scp -i /root/jenkins-pipeline/jenkins_home/.ssh/prod /tmp/.auth root@13.233.30.180:/tmp/.auth
+scp -i /root/jenkins-pipeline/jenkins_home/.ssh/prod ./jenkins/deploy/publish root@13.233.30.180:/tmp/publish
+ssh -i /root/jenkins-pipeline/jenkins_home/.ssh/prod root@13.233.30.180 "/tmp/publish"
 
